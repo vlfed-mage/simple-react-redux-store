ad02640f1da4d39fb116a442539fe6286c8d18d5
Valdo <gkorchun@gmail.com>
2023-01-25 16:38:54 +0200

    Added base styling and structure

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,16 +4,18 @@ import { Route, Switch } from "react-router-dom";
 
 import Header from "../header";
 import { CartPage, HomePage } from "../pages";
+import ShoppingCartTable from "../shopping-cart-table";
 
 const App = () => {
     return (
-        <div className='book-store' >
-            <Header />
+        <div className='container' >
+            <Header numItems={ 5 } total={ 210 } />
             <Switch>
                 <Route path='/' component={ HomePage } exact />
                 <Route path='/cart-page' component={ CartPage } />
                 <Route render={ () => <h2>404</h2> } />
             </Switch>
+            <ShoppingCartTable />
         </div>
     )
 };
