16523a420c162894c9a0965b3d64f782e6cf2263
Valdo <gkorchun@gmail.com>
2023-01-30 11:23:29 +0200

    Counting added propducts in header + dividing store into a pages

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import { NavLink } from "react-router-dom";
 
-const Header = ({ numItems, total }) => {
+const Header = ({ cartTable, cartTotal }) => {
+    const numItems = cartTable.reduce((acc, curr) => {
+        return acc + curr.count;
+    }, 0);
+
     return (
         <header className="header d-flex">
             <NavLink to='/' className="logo text-dark" >
@@ -10,10 +15,14 @@ const Header = ({ numItems, total }) => {
             </NavLink>
             <NavLink to='/cart-page' className="shopping-cart">
                 <i className="cart-icon fa fa-shopping-cart" />
-                {numItems} items (${total})
+                { numItems } items ({ `$${ cartTotal }` })
             </NavLink>
         </header>
     )
 };
 
-export default Header;
+const mapStateToProps = ({ shoppingCart: { cartTable, cartTotal } }) => {
+    return { cartTable, cartTotal };
+}
+
+export default connect(mapStateToProps)(Header);
